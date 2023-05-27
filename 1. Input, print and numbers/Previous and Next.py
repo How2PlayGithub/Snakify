9acb429d567512f1577b178a5a2ee6caaa4f4451
How2PlayGithub <how2bypass@gmail.com>
2023-05-27 20:00:47 +1200

    First commit

---
+++ 1. Input, print and numbers/Previous and Next.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3
+
+n = int(input())
+print('The next number for the number ' + str(n) + ' is ' + str(n + 1) + '.')
+print('The previous number for the number ' + str(n) + ' is ' + str(n - 1) + '.')
