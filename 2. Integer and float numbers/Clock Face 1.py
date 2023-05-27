60ee05dd3cb832cac6d1bb9b8d2b20c966e5863b
How2PlayGithub <how2bypass@gmail.com>
2023-05-27 20:23:39 +1200

    First commit

---
+++ 2. Integer and float numbers/Clock Face 1.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+
+h = int(input())
+m = int(input())
+s = int(input())
+
+print(h * 30 + m * 30 / 60 + s * 30 / 3600)
---
+++ 2. Integer and float numbers/Clock Face 2.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python3
+
+alpha = float(input())
+print(alpha % 30 * 12)
